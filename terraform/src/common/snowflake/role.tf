45f1131e5bfdf13d12330674d692ecd977897a39
Yuta Yoshimura <yuta_yoshimura@cyberagent.co.jp>
2023-08-18 17:52:00 +0900

    prepare role

--- terraform/src/common/snowflake/role.tf
+++ terraform/src/common/snowflake/role.tf
@@ -7,31 +7,121 @@ locals {
   }
 }
 
-// 開発者用のロール
-resource "snowflake_role" "developer" {
+// devの管理者のロール
+resource "snowflake_role" "dev_admin" {
   provider = snowflake.security_admin
-  name     = upper("developer")
-  comment  = "A role for developer."
+  name     = upper("dev_admin")
+  comment  = "A role for dev admin."
 }
 
-// accountadminロールを利用できるユーザーを指定
-resource "snowflake_role_grants" "accountadmin" {
+// devの開発者のロール
+resource "snowflake_role" "dev_developer" {
+  provider = snowflake.security_admin
+  name     = upper("dev_developer")
+  comment  = "A role for dev developer."
+}
+
+// devのML/DSのロール
+resource "snowflake_role" "dev_analyst" {
+  provider = snowflake.security_admin
+  name     = upper("dev_analyst")
+  comment  = "A role for dev analyst."
+}
+
+// stgの管理者のロール
+resource "snowflake_role" "stg_admin" {
+  provider = snowflake.security_admin
+  name     = upper("dev_admin")
+  comment  = "A role for dev admin."
+}
+
+// stgの開発者のロール
+resource "snowflake_role" "dev_developer" {
+  provider = snowflake.security_admin
+  name     = upper("dev_developer")
+  comment  = "A role for dev developer."
+}
+
+// stgのML/DSのロール
+resource "snowflake_role" "dev_analyst" {
+  provider = snowflake.security_admin
+  name     = upper("dev_analyst")
+  comment  = "A role for dev analyst."
+}
+
+// businessのロール
+resource "snowflake_role" "business" {
+  provider = snowflake.security_admin
+  name     = upper("business")
+  comment  = "A role for business."
+}
+
+// devの管理者ロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "dev_admin" {
   provider               = snowflake.security_admin
-  role_name              = local.snowflake_role.accountadmin
+  role_name              = snowflake_role.dev_admin.name
   enable_multiple_grants = true
   users = [
-    snowflake_user.atuy1213.name,
+    snowflake_user.admin.name,
   ]
 }
 
-resource "snowflake_role_grants" "developer" {
+// devの開発者ロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "dev_developer" {
   provider               = snowflake.security_admin
-  role_name              = snowflake_role.developer.name
+  role_name              = snowflake_role.dev_developer.name
   enable_multiple_grants = true
-
-  roles = [local.snowflake_role.sysadmin]
-  // developerを付与するユーザを指定
   users = [
-    snowflake_user.atuy1213.name,
+    snowflake_user.developer.name,
+  ]
+}
+
+// devのML/DSロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "dev_analyst" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.dev_analyst.name
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.analyst.name,
+  ]
+}
+
+// stgの管理者ロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "stg_admin" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.stg_admin.name
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.admin.name,
+  ]
+}
+
+// stgの開発者ロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "stg_developer" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.stg_developer.name
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.developer.name,
+  ]
+}
+
+// stgのML/DSロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "stg_analyst" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.stg_analyst.name
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.analyst.name,
+  ]
+}
+
+// businessのロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "business" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.business.name
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.business.name,
   ]
 }
--- terraform/src/common/snowflake/user.tf
+++ terraform/src/common/snowflake/user.tf
@@ -1,6 +1,27 @@
-resource "snowflake_user" "atuy1213" {
+resource "snowflake_user" "admin" {
   provider     = snowflake.user_admin
-  name         = "atuy1213"
-  default_role = snowflake_role.developer.name
+  name         = "admin"
+  default_role = snowflake_role.dev_admin.name
+  comment      = "Created by Terraform."
+}
+
+resource "snowflake_user" "developer" {
+  provider     = snowflake.user_admin
+  name         = "developer"
+  default_role = snowflake_role.dev_developer.name
+  comment      = "Created by Terraform."
+}
+
+resource "snowflake_user" "analyst" {
+  provider     = snowflake.user_admin
+  name         = "analyst"
+  default_role = snowflake_role.dev_analyst.name
+  comment      = "Created by Terraform."
+
+}
+resource "snowflake_user" "business" {
+  provider     = snowflake.user_admin
+  name         = "business"
+  default_role = snowflake_role.business.name
   comment      = "Created by Terraform."
 }
